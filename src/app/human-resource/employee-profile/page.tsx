2665302048c0ab625062c2c7a5e8fade0241c7e7
Asad Iqbal <asad.iqbal5165@gmail.com>
2025-08-29 04:29:53 +0500

    fixed

--- src/app/human-resource/employee-profile/page.tsx
+++ src/app/human-resource/employee-profile/page.tsx
@@ -139,8 +139,9 @@ const EmployeeProfile = () => {
             </button>
             <button
               onClick={() => {
-               // setIsModalOpen(true)
-                setState({employee_dialog:true})}
+               setIsModalOpen(true)
+                // setState({employee_dialog:true})
+              }
               }
               className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
             >
